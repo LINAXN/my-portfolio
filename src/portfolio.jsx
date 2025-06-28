8ddbb899f886f6dc7e9fba2cfb0ef0847692232c
linaxn <linaxn4@gmail.com>
2025-06-28 17:31:18 +0300

    add my project

--- src/portfolio.jsx
+++ src/portfolio.jsx
@@ -1,28 +1,33 @@
-
+import Warq from './assets/Warq.jpg';
+import Movie from './assets/Movieplanner.png';
 function App() {
+  const images = [Warq,Movie]; // هنا استخدمنا الصورة المستوردة مباشرة
+
   return (
     <div>
-  
-  
       {/* Portfolio Section */}
       <div className="portfolio_section layout_padding">
         <div className="container">
           <div className="row">
             <div className="col-sm-12">
-              <h1 className="portfolio_taital">My <span className="portfolio_taital_1">Portfolio</span></h1>
+              <h1 className="portfolio_taital">
+                My <span className="portfolio_taital_1">Portfolio</span>
+              </h1>
             </div>
           </div>
 
           <div className="portfolio_section_2">
             <div className="row">
-              {['img-1.png', 'img-2.png', 'img-3.png', 'img-4.png', 'img-5.png'].map((img, index) => (
+              {images.map((img, index) => (
                 <div className="col-md-4" key={index}>
                   <div className="container_main">
-                    <img src={`/images/${img}`} alt="" className="image" />
+                    <img src={img} alt="" className="image" />
                     <div className="overlay">
                       <div className="text">
                         <div className="btn_main">
-                          <div className="buy_bt"><a href="#">See More</a></div>
+                          <div className="buy_bt">
+                            <a href="https://github.com/AngelInvestorsPlatform/Angel-Investors-Platform">See</a>
+                          </div>
                         </div>
                       </div>
                     </div>
@@ -32,13 +37,13 @@ function App() {
             </div>
           </div>
 
-          <div className="seemore_bt"><a href="#">See More</a></div>
+          <div className="seemore_bt">
+            <a  style={{
+    marginTop: '20px'}}
+    href="#">See More</a>
+          </div>
         </div>
       </div>
-
-      
-      {/* Copyright */}
-   
     </div>
   );
 }
